72da015334436158cb7fe45b32fe141fa21bf80e
Vinicius Gibran <v-vinicius.gibran@Bryj.ai>
2024-08-14 13:44:14 +0100

    Update DataUSARepository + APIService

--- DataUSA/DataUSA/Modules/Common/Repositories/Remote/DataUSARepository.swift
+++ DataUSA/DataUSA/Modules/Common/Repositories/Remote/DataUSARepository.swift
@@ -7,17 +7,28 @@
 
 import Foundation
 
-class DataUSARepository {
+protocol DataUSARepositoryProtocol {
+    func fetchNationData() async throws -> [NationData]
+    func fetchStateData() async throws -> [StateData]
+}
+
+class DataUSARepository: DataUSARepositoryProtocol {
+    
+    private let apiService: APIServiceProtocol
+    
+    init(apiService: APIServiceProtocol = APIService.shared) {
+        self.apiService = apiService
+    }
 
     func fetchNationData() async throws -> [NationData] {
         let url = URL(string: "https://datausa.io/api/data?drilldowns=Nation&measures=Population")!
-        let response: DataUSAResponse = try await APIService.shared.performRequest(with: url)
+        let response: DataUSAResponse = try await apiService.performRequest(with: url)
         return response.data
     }
     
     func fetchStateData() async throws -> [StateData] {
         let url = URL(string: "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest")!
-        let response: StateDataResponse = try await APIService.shared.performRequest(with: url)
+        let response: StateDataResponse = try await apiService.performRequest(with: url)
         return response.data
     }
 }
--- DataUSA/DataUSA/Modules/Common/Services/APIService.swift
+++ DataUSA/DataUSA/Modules/Common/Services/APIService.swift
@@ -9,14 +9,19 @@
 import Foundation
 import Combine
 
-class APIService {
+protocol APIServiceProtocol {
+    func performRequest<T: Decodable>(with url: URL) async throws -> T
+}
+
+class APIService: APIServiceProtocol {
     static let shared = APIService()
     
     private init() {}
-
+    
     func performRequest<T: Decodable>(with url: URL) async throws -> T {
         let (data, _) = try await URLSession.shared.data(from: url)
         let decodedResponse = try JSONDecoder().decode(T.self, from: data)
         return decodedResponse
     }
 }
+
